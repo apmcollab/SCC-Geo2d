ff3b929373c49b6d899164ed9d8004acc6ee770c
Chris Anderson <anderson@math.ucla.edu>
2021-12-29 15:53:44 -0800

    added aggregate header

---
+++ SCC_Geo2d.h
@@ -0,0 +1,17 @@
+/*
+ * CAM_Geo2d.h
+ *
+ *  Created on: Dec 29, 2021
+ *      Author: anderson
+ */
+
+#ifndef SCC_GEO2D_
+#define SCC_GEO2D_
+#include "Geo2d/SCC_GeoType.h"
+#include "Geo2d/SCC_GeometricEntity.h"
+#include "Geo2d/SCC_CircleEntity.h"
+#include "Geo2d/SCC_XYrectangleEntity.h"
+#include "Geo2d/SCC_PolygonEntity.h"
+#include "Geo2d/SCC_CombinedEntity.h"
+
+#endif /* SCC_GEO2D_*/
